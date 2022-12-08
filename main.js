d823fbcd93eda4b43d65886c7ad7a1d028da353d
Doruk <110603660+Sempiller@users.noreply.github.com>
2022-12-08 21:45:30 +0300

    Ya bashar

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+var splashScreen = document.querySelector('.splash');
+splashScreen.addEventListener('click',()=>{
+  splashScreen.style.opacity = 0;
+  setTimeout(()=>{
+    splashScreen.classList.add('hidden')
+  },610)
+})
